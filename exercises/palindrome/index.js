78bb5827b944d65958cf8b6cee5b2be684528a05
Kato <kubkate@gmail.com>
2020-08-05 09:06:07 -0700

    Solve with case sensitive-PASS all tests

--- exercises/palindrome/index.js
+++ exercises/palindrome/index.js
@@ -7,6 +7,29 @@
 //   palindrome("abba") === true
 //   palindrome("abcdefg") === false
 
-function palindrome(str) {}
+
+/**
+ * Q: Case sensitive? Yes >>> A != a
+ * @param str
+ * @returns boolean
+ */
+function palindrome(str) {
+  //--------- CASE INSENSITIVE ------------
+  // make str toLowerCase store in newStr
+  let newStr = str.toLowerCase();
+  //--------------------------------------
+  // determine breaking point at mid lenght and floor down mid lenght
+  let midlen = Math.floor(str.length / 2);
+  // console.log("newStr:", newStr, 'midlen:', midlen)
+  // loop over newStr by compare a pair of beg and last
+  for (let i = 0; i < midlen; i++) {
+    // if curr i and curr j doesn't match, return false
+    if (str[i] !== str[str.length - i - 1]) return false;
+  }
+  // at the end of loop return true
+  return true;
+}
+
+// console.log(palindrome("Jbcba"));
 
 module.exports = palindrome;
