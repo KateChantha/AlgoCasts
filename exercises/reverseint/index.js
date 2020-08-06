e4516bc415edc3e1f413f5ccb2bb15e894e58118
Kato <kubkate@gmail.com>
2020-08-06 09:46:35 -0700

    Solve with provide redix in parseInt()-PASS all

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,18 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  // convert int to string then to array
+  // use array helper method
+  // join back to string 
+
+  let reverse = n.toString().split('').reverse().join('');
+  console.log(reverse);
+  // make sure to return **parse back to int with postive or negative value - using .sign()
+  return parseInt(reverse, 10) * Math.sign(n);
+}
+
+// console.log(parseInt('0070', 10))
+console.log(reverseInt(-15))
 
 module.exports = reverseInt;
