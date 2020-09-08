3b8bd6eb317ee3f9823da17993d6b481b4925469
Kato <kubkate@gmail.com>
2020-09-08 12:00:39 -0700

    Pass 2 of 2 test

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,22 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+  // create var chunked set to an empty array
+  const chunked = [];
+  // iterate over input array
+  array.forEach(element => {
+    let lastElem = chunked[chunked.length - 1]
+    // check if lastElem of chunked is not exist OR lastElem length is grater than size, if yes, create an empty array and push elem to that array
+    if (!lastElem || lastElem.length >= size) {
+      chunked.push([element]);
+    } else {
+      // if not, push element to laseELem
+      chunked.lastElem.push(element);
+    }
+  });
+  // reteurn chunked
+  return chunked;
+}
 
 module.exports = chunk;
